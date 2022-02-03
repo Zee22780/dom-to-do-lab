d37b6554aa8b64f0fe73f5f3804b02db986fa262
zuri227 <zuri22780@gmail.com>
2022-02-02 21:45:17 -0500

    Finish To-Do List

--- js/app.js
+++ js/app.js
@@ -2,3 +2,14 @@ const taskItem = document.getElementById("task");
 const btn = document.getElementById("submit-button");
 const list = document.getElementById("todo-list");
 
+btn.addEventListener("click", function(evt){
+  const newLi = document.createElement("li")
+  const inp = document.querySelector("input")
+  newLi.textContent = inp.value
+  if(inp.value !== ""){
+    document.querySelector("ul").appendChild(newLi)
+    inp.value = "";
+    console.log(inp);
+  }
+});
+
